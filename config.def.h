82d66770316d909a7660fcf864dff150eb109e4a
audioz <73185455+ignaudioz@users.noreply.github.com>
2023-05-16 02:07:16 +0300

    I think, I fixed it.

--- config.def.h
+++ config.def.h
@@ -36,12 +36,12 @@ static const Rule rules[] = {
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	/* { "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 }, */
-	{ "LibreWolf", NULL,     NULL,           1,    0,          0,          -1,        -1 },
-	{ "Brave-browser", NULL,     NULL,           1,    0,          0,          -1,        -1 },
-	{ "discord", NULL,     NULL,           3,    0,          0,          -1,        1 },
-	{ "Alacritty", NULL,     NULL,           2,    0,          0,          -1,        -1 },
-	{ "mpv", NULL,     NULL,           0,    0,          1,          -1,        -1 },
-	{ "thunar", NULL,     NULL,           0,    1,          0,          -1,        -1 },
+	{ "LibreWolf", NULL,     NULL,           1,    0,          false,          -1,        -1 },
+	{ "Brave-browser", NULL,     NULL,           1,    false,          0,          -1,        -1 },
+	{ "discord", NULL,     NULL,          1 << 2,    0,          false,          -1,        1 },
+	{ "Alacritty", NULL,     NULL,          1 << 1,    0,          false,          -1,        -1 },
+	{ "mpv", NULL,     NULL,           0,    0,          true,          -1,        -1 },
+	{ "thunar", NULL,     NULL,           0,    1,          false,          -1,        -1 },
 	/* { "St",      NULL,     NULL,           0,         0,          1,           0,        -1 }, */
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
